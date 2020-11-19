8cf46dfc2c48730d2782c02b2577538c38bc8cf7
alina <alinahancharova@gmail.com>
2020-11-19 18:46:57 +0300

    create database diploma_project

--- diploma_project/diploma_project/settings.py
+++ diploma_project/diploma_project/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'diploma_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "diploma_poject",
+        "HOST": "localhost",
+        "PORT": 5432,
+        "USER": "postgres",
+        "PASSWORD": "qwerty"
     }
 }
 
